652316bf1c5a2ccdd40ecb17d67154b910e8a084
kael <kaelwebdev@gmail.com>
2024-03-19 17:01:07 -0500

    color correction. Also fixed responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+//const colors = require('tailwindcss/colors')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
-				/* primaryA: "#0369a1",
-				secondaryA: "var(--color-secondary)",
-				primaryB: "#0369a1",
-				secondaryB: "var(--color-secondary)", */
+				/* color_theme_light_title: colors.sky,
+				color_theme_dark_title: colors.yellow,  */
 			},
 		},
 	},
